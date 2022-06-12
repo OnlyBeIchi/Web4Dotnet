a5ff221de8a46990d4b605a2d9e13e11c48c101b
OnlybeIchi <Admin@DESKTOP-78K56LL>
2022-06-12 16:25:32 +0700

    Xóa không cần thiết

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,11 +12,6 @@ namespace Web4dotnet.Controllers
     public class HomeController : Controller
     {
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult About()
         {
             return View();
